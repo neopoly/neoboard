3e8fbf8182afb7b197c8b1101198a59d60a0004b
Jonas Thiel <jt@neopoly.de>
2015-05-13 16:49:07 +0200

    Use bower for vendor'd filed

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 To start your new Phoenix application:
 
 1. Install dependencies with `mix deps.get`
-2. Start Phoenix endpoint with `mix phoenix.server`
+2. Initialize node with `npm install`
+3. Initialize bower with `bower install`
+4. Start Phoenix endpoint with `mix phoenix.server`
 
 Now you can visit `localhost:4000` from your browser.
--- brunch-config.js
+++ brunch-config.js
@@ -2,7 +2,10 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: 'js/app.js'
+      joinTo: {
+        'js/app.js': /^web\/static\/js/,
+        'js/vendor.js': /^(web\/static\/vendor|bower_components)/
+      }
       // To use a separate vendor.js bundle, specify two files path
       // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
       // joinTo: {
@@ -40,7 +43,7 @@ exports.config = {
   plugins: {
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/^(web\/static\/vendor)/]
+      ignore: [/^(web\/static\/vendor|node_modules|bower_components)/]
     }
   }
 };
