f97880e5414c393036f5397e84e7677d0f8dcc9e
Jonas Thiel <jt@neopoly.de>
2015-05-13 16:29:39 +0200

    Add some styling for time widget

--- web/static/js/widgets/time.js
+++ web/static/js/widgets/time.js
@@ -1,23 +1,54 @@
 import WidgetMixin from "../widget_mixin"
 
-const d = function(number){
+const d = function(number) {
   return number < 10 ? "0"+number : number
 }
 
-const Time = React.createClass({
-  getDefaultProps(){
+const days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+const weekday = function(day) {
+  return days[day]
+}
+
+const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+const month = function(month) {
+  return months[month]
+}
+
+const MyTime = React.createClass({
+  getDefaultProps() {
     return {
       sep: ":"
     }
   },
-  render(){
+  render() {
     return (
-      <div>
+      <div className="Time">
         <span>{d(this.props.h)}</span>
-        <span>{d(this.props.sep)}</span>
+        <span>{this.props.sep}</span>
         <span>{d(this.props.m)}</span>
-        <span>{d(this.props.sep)}</span>
+        <span>{this.props.sep}</span>
         <span>{d(this.props.s)}</span>
+      </div>
+    );
+  }
+})
+
+const MyDate = React.createClass({
+  getDefaultProps() {
+    return {
+      sep: " "
+    }
+  },
+  render() {
+    return (
+      <div className="Date">
+        <span>{weekday(this.props.w)}</span>
+        <span>{this.props.sep}</span>
+        <span>{month(this.props.m)}</span>
+        <span>{this.props.sep}</span>
+        <span>{d(this.props.d)}</span>
+        <span>{this.props.sep}</span>
+        <span>{d(this.props.y)}</span>
       </div>
     );
   }
@@ -26,12 +57,16 @@ const Time = React.createClass({
 export default React.createClass({
   mixins: [WidgetMixin("time:state")],
   getInitialState() {
-    return { time: {h: 0, m: 0, s: 0} }
+    return {
+      time: {h: 0, m: 0, s: 0},
+      date: {d: 0, m: 0, y: 0}
+    }
   },
   render() {
     return (
-      <div className="WidgetTime">
-        <Time {...this.state.time}/>
+      <div className="TimeWidget">
+        <MyDate {...this.state.date}/>
+        <MyTime {...this.state.time}/>
       </div>
     );
   },
@@ -42,6 +77,12 @@ export default React.createClass({
         h: now.getHours(),
         m: now.getMinutes(),
         s: now.getSeconds()
+      },
+      date: {
+        d: now.getDate(),
+        w: now.getDay(),
+        m: now.getMonth(),
+        y: now.getFullYear()
       }
     }
   }
