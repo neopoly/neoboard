d695699df9c07723c05bc8cfcdb61479f13e9bbe
Jonas Thiel <jt@neopoly.de>
2015-05-19 12:09:47 +0200

    Add test for TimeService

---
+++ test/neoboard/time_service_test.exs
@@ -0,0 +1,17 @@
+defmodule Neoboard.TimeServiceTest do
+  use ExUnit.Case, async: true
+
+  test "it determines now" do
+    assert %Timex.DateTime{} = Neoboard.TimeService.now
+  end
+
+  test "it returns now formatted" do
+    formatted = Neoboard.TimeService.now("{RFC1123}")
+    assert formatted =~ ~r/\w{3}, \d{1,2} \w{3} \d{4} \d{2}:\d{2}:\d{2}\.?\d* \w{2,3}/
+  end
+
+  test "it returns now as iso" do
+    formatted = Neoboard.TimeService.now_as_iso
+    assert formatted =~ ~r/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.?\d*\+\d{4}/
+  end
+end
