7eac0ba56cbd6cc12f0565c64ea1cfa0abaa6553
Jonas Thiel <jt@neopoly.de>
2015-09-04 15:40:35 +0200

    Allow cross-origin socket connections for now

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Neoboard.UserSocket do
   channel "*", Neoboard.BoardChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    check_origin: false
   transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
