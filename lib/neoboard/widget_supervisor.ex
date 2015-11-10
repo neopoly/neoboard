2463c5621b9cfed071eab763cf8bec9c4692e608
Jonas Thiel <jt@neopoly.de>
2015-11-10 15:19:52 +0100

    Start Widgets in a ProcessCushion

--- lib/neoboard/widget_supervisor.ex
+++ lib/neoboard/widget_supervisor.ex
@@ -1,5 +1,9 @@
 defmodule Neoboard.WidgetSupervisor do
+  alias Neoboard.ProcessCushion
   use Supervisor
+
+  @delay 10_000 # at least 10 seconds for exiting widgets
+  @shutdown_timeout 1_000 # wait 1 second when shutting down widgets
 
   def start_link do
     Supervisor.start_link(__MODULE__, [])
@@ -8,10 +12,15 @@ defmodule Neoboard.WidgetSupervisor do
   def init([]) do
     cond do
       Neoboard.Widgets.auto_start? ->
-        children = Enum.map(Neoboard.Widgets.enabled, &(worker(&1, [])))
+        children = Enum.map(Neoboard.Widgets.enabled, &worker_cushion/1)
       true ->
         children = []
     end
     supervise(children, strategy: :one_for_one, max_restarts: 20)
+  end
+
+  defp worker_cushion(module) do
+    id = "#{module}Cushion"
+    worker(ProcessCushion, [id, @delay, @shutdown_timeout, module], id: id)
   end
 end
