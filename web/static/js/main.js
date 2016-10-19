924591170237e13459da4e000ea603e6ae38d5f9
Jonas Thiel <jt@neopoly.de>
2016-10-19 16:48:45 +0200

    Add mattermost widget

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -8,6 +8,7 @@ import NotepadWidget from "./widgets/notepad"
 import NichtLustigWidget from "./widgets/nicht_lustig"
 import RedmineProjectTable from "./widgets/redmine_project_table"
 import Gitter from "./widgets/gitter"
+import Mattermost from "./widgets/mattermost"
 import RedmineActivityProjects from "./widgets/redmine_activity_projects"
 import RedmineActivityUsers from "./widgets/redmine_activity_users"
 import OwncloudImages from "./widgets/owncloud_images"
@@ -16,7 +17,7 @@ import Images from "./widgets/images"
 const widgets = [
   // Format:
   // [widget, grid configuration]
-  [Gitter,                  {x:0, y:0, w:1, h:2}],
+  [Mattermost,              {x:0, y:0, w:1, h:2}],
   [RedmineActivityProjects, {x:1, y:0, w:1, h:2}],
   [RedmineActivityUsers,    {x:2, y:0, w:1, h:1}],
   [OwncloudImages,          {x:4, y:1, w:1, h:1}],
